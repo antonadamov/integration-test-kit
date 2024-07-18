62903b10219b428114d8a88210896395e5015b56
antonadamov <anton.adamau@gmail.com>
2024-07-17 19:50:07 -0700

    configuration loading was implemented

--- src/main/java/com/example/config/TestConfig.java
+++ src/main/java/com/example/config/TestConfig.java
@@ -1,8 +1,13 @@
 package com.example.config;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "com.example")
+@EnableAutoConfiguration
 public class TestConfig {
+
+
+
 }
