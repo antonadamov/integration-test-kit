c2b36fd3ee62ff47fc5ca4cdecb97fbec0cbfc61
Anton Adamov <anton.adamau@gmail.com>
2024-07-23 21:21:44 -0700

    wrapper for get users request of test service was added

---
+++ src/main/java/com/example/rest/GetUsersRequest.java
@@ -0,0 +1,32 @@
+package com.example.rest;
+
+import io.restassured.response.Response;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class GetUsersRequest {
+
+    private Response response;
+
+    public Response send() {
+        if (response == null){
+            response = given()
+                    .header("Content-Type", "application/json")
+                    .when()
+                    .get("/users");
+        }
+        return response;
+    }
+
+    public List<String> getUsersData() {
+        if (response == null){
+            this.send();
+        }
+        return response
+                .jsonPath()
+                .getList(".", String.class);
+    }
+
+}
