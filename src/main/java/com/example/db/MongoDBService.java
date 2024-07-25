6a5a1b398c90b2dfc8ef26990749a15d00f763b9
Anton Adamov <anton.adamau@gmail.com>
2024-07-24 20:17:39 -0700

    DB service functionality was improved

--- src/main/java/com/example/db/MongoDBService.java
+++ src/main/java/com/example/db/MongoDBService.java
@@ -10,29 +10,85 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 @Slf4j
 public class MongoDBService {
 
     private final MongoClient mongoClient;
+    private MongoDatabase database;
 
 
     @Value("${spring.mongodb.database}")
     private String dbName;
 
+    @Value("${spring.mongodb.polling.interval:1000}")
+    private long pollingInterval;
+
+    @Value("${spring.mongodb.polling.timeout:30000}")
+    private long pollingTimeout;
+
     @Autowired
     public MongoDBService(MongoClient mongoClient) {
         this.mongoClient = mongoClient;
+        this.database = mongoClient.getDatabase(dbName);
     }
 
-    public Document getDocument(String collectionName, String documentId) {
+    public DbDocument getDocument(String collectionName, String documentId) throws AsyncTestingFrameworkException {
         try {
-            MongoDatabase database = mongoClient.getDatabase(dbName);
             MongoCollection<Document> collection = database.getCollection(collectionName);
-            return collection.find(new Document("_id", documentId)).first();
+            return (DbDocument) collection.find(new Document("_id", documentId)).first();
         } catch (Exception e) {
             throw new AsyncTestingFrameworkException("An exception occurred while getting a document from DB", e);
         }
+    }
+
+    public DbDocument getDocument(String collectionName, Map<String, Object> fieldValues) throws AsyncTestingFrameworkException {
+        try {
+            MongoCollection<Document> collection = database.getCollection(collectionName);
+            Document query = new Document();
+            for (Map.Entry<String, Object> entry : fieldValues.entrySet()) {
+                query.append(entry.getKey(), entry.getValue());
+            }
+            return (DbDocument) collection.find(query).first();
+        } catch (Exception e) {
+            throw new AsyncTestingFrameworkException("An exception occurred while getting a document from DB", e);
+        }
+    }
+
+    public DbDocument waitForDocument(String collectionName, String documentId) throws AsyncTestingFrameworkException {
+        long startTime = System.currentTimeMillis();
+        while (System.currentTimeMillis() - startTime < pollingTimeout) {
+            DbDocument document = getDocument(collectionName, documentId);
+            if (document != null) {
+                return document;
+            }
+            try {
+                Thread.sleep(pollingInterval);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new AsyncTestingFrameworkException("Polling interrupted while waiting for document", e);
+            }
+        }
+        throw new AsyncTestingFrameworkException("Timeout while waiting for document to appear in DB");
+    }
+
+    public DbDocument waitForDocument(String collectionName, Map<String, Object> fieldValues) throws AsyncTestingFrameworkException {
+        long startTime = System.currentTimeMillis();
+        while (System.currentTimeMillis() - startTime < pollingTimeout) {
+            DbDocument document = getDocument(collectionName, fieldValues);
+            if (document != null) {
+                return document;
+            }
+            try {
+                Thread.sleep(pollingInterval);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new AsyncTestingFrameworkException("Polling interrupted while waiting for document", e);
+            }
+        }
+        throw new AsyncTestingFrameworkException("Timeout while waiting for document to appear in DB");
     }
 
 
